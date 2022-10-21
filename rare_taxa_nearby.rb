37c545631fb1c0c7246b63ea87247b8c8e310421
= <=>
2022-10-20 20:56:54 -0700

    Add ability to filter observations by month

--- rare_taxa_nearby.rb
+++ rare_taxa_nearby.rb
@@ -6,12 +6,18 @@ class RareTaxaNearby
   TAXA_PER_PAGE = 200
   NUM_RAREST_TO_DISPLAY = 100
   SEARCH_RANGE = 0.2 # degrees latitude and longitude around center
+  EXCLUDE_RANKS = %w[hybrid].freeze
 
   def run
-    get_location
-    get_parent_taxon
+    collect_inputs
     fetch_taxa
     display_results
+  end
+
+  def collect_inputs
+    get_location
+    get_parent_taxon
+    get_month
   end
 
   def get_location
@@ -27,15 +33,20 @@ class RareTaxaNearby
     @parent_taxon_id = gets.chomp
   end
 
+  def get_month
+    puts 'Only include observations from these months ("10,11" = October and November, leave blank for any)'
+    @months = gets.chomp
+  end
+
   def fetch_taxa
     @taxa = []
-    puts "Looking up taxa #{"within taxon_id #{@parent_taxon_id} " if @parent_taxon_id}observed near  #{@latitude}, #{@longitude}"
+    puts "Looking up taxa #{"within taxon_id #{@parent_taxon_id} " if @parent_taxon_id}#{"from the months #{@months} " if @months}observed near  #{@latitude}, #{@longitude}"
     total_taxa = get_local_taxa_count
     puts "There are #{total_taxa} that have been observed in your area!"
 
     (total_taxa.to_f / TAXA_PER_PAGE).ceil.times do |page|
       puts "Fetching taxa #{(page * TAXA_PER_PAGE) + 1} to #{(page + 1) * TAXA_PER_PAGE}"
-      uri = URI("#{base_url}&page=#{page + 1}&per_page=#{TAXA_PER_PAGE}#{"&taxon_id=#{@parent_taxon_id}" if @parent_taxon_id}")
+      uri = URI("#{base_url}&page=#{page + 1}&per_page=#{TAXA_PER_PAGE}")
       request_result = JSON.parse(Net::HTTP.get(uri))
       @taxa << request_result['results']
     end
@@ -45,6 +56,7 @@ class RareTaxaNearby
 
   def display_results
     puts "Here are the #{NUM_RAREST_TO_DISPLAY} rarest taxa near #{@latitude}, #{@longitude}"
+    @taxa.reject! { |taxon| EXCLUDE_RANKS.include?(taxon.rank) }
     @taxa.sort! { |a, b| a.total_observations <=> b.total_observations }
 
     NUM_RAREST_TO_DISPLAY.times do |num|
@@ -56,11 +68,21 @@ class RareTaxaNearby
   private
 
   def base_url
-    @base_url ||= "https://api.inaturalist.org/v1/observations/species_counts?verifiable=true&nelat=#{max_latitude}&nelng=#{max_longitude}&swlat=#{min_latitude}&swlng=#{min_longitude}&locale=en"
+    @base_url ||= "https://api.inaturalist.org/v1/observations/species_counts?verifiable=true&nelat=#{max_latitude}&nelng=#{max_longitude}&swlat=#{min_latitude}&swlng=#{min_longitude}&locale=en#{taxon_query}#{month_query}"
+  end
+
+  def taxon_query
+    return '' unless @parent_taxon_id
+    "&taxon_id=#{@parent_taxon_id}"
+  end
+
+  def month_query
+    return '' unless @months
+    "&month=#{@months}"
   end
 
   def get_local_taxa_count
-    uri = URI("#{base_url}&page=1&per_page=1#{"&taxon_id=#{@parent_taxon_id}" if @parent_taxon_id}")
+    uri = URI("#{base_url}&page=1&per_page=1")
     basic_details = JSON.parse(Net::HTTP.get(uri))
     basic_details['total_results']
   end
